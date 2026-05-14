371e97c00bafe0ea4000753184a3aa6c6b4d1c77
AayushMandavia <aayushmandavia1412@gmail.com>
2026-05-14 13:22:03 +0530

    Fix CORS issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Distributed Task Orchestrator")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://queuenest.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
